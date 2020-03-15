773ec4ee49533bfb50b1a1bf36d8ef8ff788b2d4
greenbotgo <46903152+greenbotgo@users.noreply.github.com>
2020-03-15 15:09:14 -0400

    update Sonic_Test.ino

--- example/Sonic_Test/Sonic_Test.ino
+++ example/Sonic_Test/Sonic_Test.ino
@@ -1,11 +1,18 @@
 #include <Sonic.h>
 
-//Declare Sonic object. 
+//Declare Sonic object. Then for the parameters choose two IO pins
+//for your Ultrasonic range finder's 'Trig' and 'Echo' pins.
+//Timeout is set to 22000UL by default. That is the amount of time it takes
+//sound to travel 375cm and bounce off an object and travel 375cm back to the 
+//ultrasound receiver.
+//If you wish to change that based on your modules accuracy just add the
+//amount of microseconds in the parameters of the Sonic object declaration.
 //Sonic sonic(<Trigger pin>, <Echo pin>, <Timeout>);
 Sonic sonic(7,8);
 
 void setup() {
   Serial.begin(9600);
+  
   sonic.setTimeout(22000UL);
 }
 
